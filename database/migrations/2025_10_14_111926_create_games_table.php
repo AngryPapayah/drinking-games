680f1cb596558321b509e979a0c9a1e9101e2e00
AngryPapayah <airissavermo@hotmail.com>
2025-10-15 12:21:12 +0200

    image changelog toegevoegd

---
+++ database/migrations/2025_10_14_111926_create_games_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('game_type_id');
+            $table->string('name');
+            $table->string('description');
+            $table->tinyInteger('total_players');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
